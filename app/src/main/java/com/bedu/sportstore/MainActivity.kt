adb30fc3825eace90ba43fd89f8bb1c34ba1f128
lilianaRodriguezRodriguez <dgliliana.rodriguez@gmail.com>
2023-05-10 01:15:20 -0500

    fragments para bottom_bar_menu

--- app/src/main/java/com/bedu/sportstore/MainActivity.kt
+++ app/src/main/java/com/bedu/sportstore/MainActivity.kt
@@ -2,10 +2,42 @@ package com.bedu.sportstore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.bedu.sportstore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNavigationView.setOnItemReselectedListener {
+
+            when(it.itemId){
+
+                R.id.home -> replaceFragment(Home())
+                R.id.search -> replaceFragment(Search())
+                R.id.car -> replaceFragment(Car())
+                R.id.settings -> replaceFragment(Settings())
+
+                else ->{
+
+                }
+            }
+        }
+                true
+
+
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_Layout,fragment)
+        fragmentTransaction.commit()
+
     }
 }
