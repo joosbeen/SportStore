745d20d382a64fbd30302e5198bd4de4587799d7
joosbeen <joosbeen@gmail.com>
2023-05-11 15:21:27 -0600

    Se agrego fragment forma_pago

---
+++ app/src/main/java/com/bedu/sportstore/ui/fragments/main/FormaPagoFragment.kt
@@ -0,0 +1,20 @@
+package com.bedu.sportstore.ui.fragments.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bedu.sportstore.R
+
+class FormaPagoFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_forma_pago, container, false)
+    }
+
+}
